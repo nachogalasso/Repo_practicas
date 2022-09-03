85c9523b8f7cdcd12c8aebac8174dc4d90ee2cb4
nachogalasso <ignaciogalasso82@gmail.com>
2022-09-03 21:42:09 +0200

    templates start

--- practica_JS/js_api/indexDB/scripts.js
+++ practica_JS/js_api/indexDB/scripts.js
@@ -2,9 +2,10 @@
 // por lo tanto tenemos que poner window.indexedDB, de lo contrario nos dará fallo. Si lo llamamos idb, no tenemos que poner window
 // esto sucede porque la constante donde lo guardamos se llama igual.
 const indexedDB = window.indexedDB
+const form = document.getElementById('form');
 
 // lo primero es preguntar si existe indexedDB, creamos una variable en la que vamos a almacenar los datos
-if(indexedDB) {
+if(indexedDB && form) {
   let db
   // para acceder a la base de datos utilizamos el siguiente comando, recibe 2 parámetros, el nombre y el otro podemos poner version
   // los números tienen que ser siempre enteros, si creamos distintas versiones, crea distintas bases de datos
@@ -21,13 +22,43 @@ if(indexedDB) {
   request.onupgradeneeded = () => {
     db = request.result;
     console.log("Create", db);
-    const objectStore = db.createObjectStore('tasks')
+    const objectStore = db.createObjectStore('tasks', {
+      autoIncrement: true
+      // es importante tratar de usar números únicos, como el dni de una persona. El titulo una tarea tmb puede ser. Otro que se puede
+      // usar es keyPath: 'taskTitle'
+    })
   }
 
   request.onerror = (error) => {
     console.log('Error', error) // error lo pasamos como parámetro en el caso de que haya un error
   }
 
+  // creamos una función para agregar los datos, y el parámetro que vamos a pasar será data. Se hace en 3 pasos y tmb trabajamos con
+  // parámetros, en el 1er paso transaction van 2: 1ro el almacén de datos 'tasks' y el 2do es el modo de la transacción: 
+  // readonly, solamente para leer y readwrite para escribir datos. Si no ponemos nada por defecto es readonly
+  const addData = (data) => {
+    const transaction = db.transaction(['tasks'], 'readwrite')
+    // Ahora que tenemos la transacción creada, tenemos que abrir el almacén de datos, que se encuentra en transaction y usamos objectStore
+    // que recibe como parámetro el almacén con el cual vamos a trabajar que es 'tasks'
+    const objectStore = transaction.objectStore('tasks')
+    // luego de hecho esto le tenemos que indicar que añada los datos. add() y los parámetros son los que llegan por data
+    const request = objectStore.add(data)
+    // Si da error es porque no se le están asignando las keys correspondientes. Se necesita de una clave. Con autoIncrement: true
+    // colocandolo en el create, le indicamos al programa que cree la key por si mismo. Tener en cuenta que si ya tenemos creado el
+    // almacén de datos, nos dará nuevamente error. Podemos eliminar el anterior  
+  }
+
   // Cuando vemos que abrió la base y está todo correcto, lo siguiente es crear el almacen de datos, todas las funciones se realizan
   // en el método onepgradeneeded, recordemos que estaremos actualizando y modificando la misma
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    // creamos el objeto para enviar a la base de datos, con task y priority accedemos al input y el select
+    const data = {
+      taskTitle:e.target.task.value,
+      taskPriority:e.target.priority.value
+    }
+    console.log(data.taskTitle, data.taskPriority)
+    console.log(data)
+    addData(data)
+  })
 }
